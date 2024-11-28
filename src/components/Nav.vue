0e901b7994d2c6ad1fd101eb1cd0819eea82a8e8
Firet <damianfederico@protonmail.com>
2024-11-28 00:04:19 -0300

    Add styles to header

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,15 +1,45 @@
 <script setup>
-import { useAuthStore } from '@/stores';
+import { useAuthStore } from "@/stores";
 
 const authStore = useAuthStore();
 </script>
 
 <template>
-    <nav v-show="authStore.user" class="navbar navbar-expand navbar-dark bg-dark">
-        <div class="navbar-nav">
-            <router-link to="/" class="nav-item nav-link">Home</router-link>
-            <router-link to="/users" class="nav-item nav-link">Users</router-link>
-            <button @click="authStore.logout()" class="btn btn-link nav-item nav-link">Logout</button>
-        </div>
-    </nav>
+  <!-- <nav v-show="authStore.user" class="navbar navbar-expand navbar-dark bg-dark"> -->
+  <nav v-show="authStore.user">
+    <div class="header navbar-nav">
+      <img
+        class="logo"
+        src="/provincia-seguros-imagotipo.jpg"
+        alt="Provincia Seguros Logo"
+      />
+
+      <router-link to="/" class="">Home</router-link>
+      <router-link to="/users" class="">Users</router-link>
+      <div class="header-item">Seguros Personas</div>
+      <div>Seguros Empresas</div>
+      <div>Proveedores</div>
+      <div>Quienes Somos</div>
+      <div>Nuestras Sedes</div>
+      <button @click="authStore.logout()" class="">Logout</button>
+    </div>
+  </nav>
 </template>
+
+<style>
+.header {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-evenly;
+  align-items: center;
+  background-color: white;
+  color: black;
+  border: solid 1px black;
+  width: 100%;
+  height: 100px;
+}
+.logo {
+  width: 100px;
+  height: 100px;
+}
+</style>
